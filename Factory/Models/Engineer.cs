50c500630faa924ffe40681e175094b1f3f5328b
Jake Elsberry <jelsberry96@gmail.com>
2023-10-14 21:33:54 -0700

    data annotations for engineer WIP

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,6 +6,7 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "* You must name an engineer.")]
     public string Name { get; set; }
     public List <EngineerMachine> EngineerMachines { get; set; }
   }
